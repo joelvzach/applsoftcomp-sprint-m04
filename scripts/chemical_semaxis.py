0e4bca5c90698eebbc86f62a80bb23bf35832efc
cameron-kranz <cameronkranz2@gmail.com>
2026-04-16 18:08:06 +0000

    made assignment file and added imports

---
+++ scripts/chemical_semaxis.py
@@ -0,0 +1,7 @@
+import marimo as mo
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import altair as alt
+from sentence_transformers import SentenceTransformer
+from drawdata import ScatterWidget
